388d60c5cc75b87f4dff8752e65c9173b6eed4ce
Rui Chen <rui@chenrui.dev>
2022-04-26 10:46:24 -0400

    bump revision for docker image clash

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -4,6 +4,7 @@ class Testkube < Formula
   url "https://github.com/kubeshop/testkube/archive/refs/tags/v1.0.11.tar.gz"
   sha256 "c1a6e14088657523f2527a655b0d995ec0a976c9230370be04506422180c8214"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9a506e76afb31fd1fdc6e6b514fa587dc1587102721aeddd5c99462a257ac2ef"
